bde89d8866d64461cab1060f429156b7aec50e91
Steven Meyers <sjjmeyers@gmail.com>
2020-04-07 20:49:20 -0600

    Add logout test.

--- spec/features/visitor/login_spec.rb
+++ spec/features/visitor/login_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor when I visit the login page" do
-  xit "if I am a regular user I'm redirected to my profile page when I submit valid information" do
+  it "if I am a regular user I'm redirected to my profile page when I submit valid information" do
     user = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -18,7 +18,7 @@ RSpec.describe "As a visitor when I visit the login page" do
     expect(current_path).to eq("/profile")
   end
 
-  xit "if I am a merchant user I'm redirected to my merchant dashboard page when I submit valid information" do
+  it "if I am a merchant user I'm redirected to my merchant dashboard page when I submit valid information" do
     merchant = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -36,7 +36,7 @@ RSpec.describe "As a visitor when I visit the login page" do
     expect(current_path).to eq("/merchant/dashboard")
   end
 
-  xit "if I am an admin user I'm redirected to my admin dashboard page when I submit valid information" do
+  it "if I am an admin user I'm redirected to my admin dashboard page when I submit valid information" do
     admin = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -52,7 +52,7 @@ RSpec.describe "As a visitor when I visit the login page" do
     click_button "Login"
   end
 
-  xit "if I am any user I see a flash message saying that I am logged in" do
+  it "if I am any user I see a flash message saying that I am logged in" do
     user = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -70,7 +70,7 @@ RSpec.describe "As a visitor when I visit the login page" do
     expect(page).to have_content("You are now logged in!")
   end
 
-  xit "if I enter bad credentials I see a flash message saying that I entered an invalid email or password" do
+  it "if I enter bad credentials I see a flash message saying that I entered an invalid email or password" do
     user = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -94,7 +94,7 @@ RSpec.describe "As a visitor when I visit the login page" do
     expect(page).to have_content("You entered incorrect credentials.")
   end
 
-  it "if I am a regular user and already logged in I am redirected to my profile page and see a flash message" do
+  it "if I am a user and already logged in I am redirected to my profile page and see a flash message" do
     user = User.create!(name: "Steve",
                         address:"123 Main St.",
                         city: "Fort Collins",
@@ -113,5 +113,31 @@ RSpec.describe "As a visitor when I visit the login page" do
     visit "/login"
     expect(current_path).to eq("/profile")
     expect(page).to have_content("You are already logged in!")
+  end
+
+  it "if I am a user and I am logged in I can log out, and get returned to the welcome page, and the cart is empty" do
+    mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+
+    user = User.create!(
+      name: "Steve",
+      address:"123 Main St.",
+      city: "Fort Collins",
+      state: "GA",
+      zip: "66666",
+      email: "chunky_lover@example.com",
+      password: "123password")
+
+    visit "/login"
+    fill_in :Email, with: user.email
+    fill_in :Password, with: user.password
+    click_button "Login"
+
+    visit "/items/#{paper.id}"
+    click_on "Add To Cart"
+    click_on "Logout"
+    expect(current_path).to eq("/")
+    expect(page).to have_content("Cart: 0")
+    expect(page).to have_content("You have been sucessfully logged out. Bye!")
   end
 end
