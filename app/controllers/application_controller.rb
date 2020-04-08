d8b3052df43c32bf39867f0e60a4b03d575394b3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-08 14:24:06 -0600

    Add admin authorization helper methods to application controller and admin dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_merchant?
+  helper_method :cart, :current_user, :current_merchant?, :current_admin?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -13,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def current_merchant?
     current_user && current_user.merchant?
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
   end
 end
