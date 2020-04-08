a136270c9f627b1e1254c08462362879dc5b6dd9
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-08 14:40:10 -0600

    add us3 user navigation and testing

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -53,4 +53,41 @@ RSpec.describe 'Site Navigation' do
 
     end
   end
+
+  describe "as a user i see all visitor links plus" do
+    context "i see links to /profile and /logout and" do
+      context "i do not see links to /login and /register and" do
+        it "i see 'Logged in as <username>'" do
+
+          user = User.create!(
+            name: "Steve",
+            address:"123 Main St.",
+            city: "Fort Collins",
+            state: "GA",
+            zip: "66666",
+            email: "chunky_lover@example.com",
+            password: "123password"
+          )
+
+          visit "/login"
+          fill_in :Email, with: user.email
+          fill_in :Password, with: user.password
+          click_button "Login"
+
+          within 'nav' do
+            expect(page).to have_link('Home')
+            expect(page).to have_link('All Items')
+            expect(page).to have_link('All Merchants')
+            expect(page).to have_link('Cart: 0')
+            expect(page).to have_link('Profile')
+            expect(page).to have_link('Logout')
+            expect(page).to have_no_link('Register')
+            expect(page).to have_no_link('Login')
+            expect(page).to have_content("Logged in as #{user.name}")
+          end
+
+        end
+      end
+    end
+  end
 end
