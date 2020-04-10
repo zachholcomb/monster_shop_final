0984b3e8478de7478813ee5560d07ffab728b5fd
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-10 12:36:10 -0600

    Add test for checkout error flash message

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -19,6 +19,18 @@ RSpec.describe 'Cart show' do
     end
 
     it 'Theres a link to checkout' do
+      user = User.create!(name: "Steve",
+                          address:"123 Main St.",
+                          city: "Fort Collins",
+                          state: "GA",
+                          zip: "66666",
+                          email: "chunky_lover@example.com",
+                          password: "123password")
+      visit "/login"
+      fill_in :Email, with: user.email
+      fill_in :Password, with: user.password
+      click_button "Login"    
+                      
       visit "/cart"
 
       expect(page).to have_link("Checkout")
@@ -34,6 +46,41 @@ RSpec.describe 'Cart show' do
       visit "/cart"
 
       expect(page).to_not have_link("Checkout")
+    end
+  end
+
+  describe "As a vistor" do
+    before(:each) do
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@tire.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+      @items_in_cart = [@paper,@tire,@pencil]
+    end
+    context "When I have items in my cart" do
+      it "I have to either login or register to checkout" do
+        visit "/cart"
+        expect(page).to have_content('To checkout you must log in or register')
+        expect(page).to have_link('register')
+        expect(page).to have_link('log in')
+        click_link('register')
+        expect(page).to have_current_path('/register')
+        visit "/cart"
+        click_link('log in')
+        expect(page).to have_current_path('/login')
+        visit "/cart"
+        click_link('Checkout')
+        expect(page).to have_current_path('/cart')
+        expect(page).to have_content('Unable to checkout, please log in or register.')
+      end
     end
   end
 end
