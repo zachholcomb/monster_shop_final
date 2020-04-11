c798781dd99fec7279e449481ca94b7f06cdc94e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-11 11:06:39 -0600

    Fix bug in error messages in user register

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest, require: true
+  after_validation { self.errors.messages.delete(:password_digest) }
   has_secure_password
   enum role: %w(default merchant admin)
 end
