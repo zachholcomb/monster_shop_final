c2b4d81ff99a57c798143a30fa1f5e7098a4ee77
Steven Meyers <sjjmeyers@gmail.com>
2020-04-11 17:36:06 -0600

    Add My Orders link to profile page

--- spec/features/user/profile_spec.rb
+++ spec/features/user/profile_spec.rb
@@ -43,5 +43,42 @@ RSpec.describe "As a registered user" do
 
       expect(page).to have_link("Edit My Profile")
     end
+    
+    it "and I see a link to all of my orders if there are any" do
+      user = User.create!(name: "Steve",
+                          address:"123 Main St.",
+                          city: "Fort Collins",
+                          state: "GA",
+                          zip: "66666",
+                          email: "chunky_lover@example.com",
+                          password: "123password")
+
+      visit "/login"
+      fill_in :Email, with: user.email
+      fill_in :Password, with: user.password
+      click_button "Login"
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@tire.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+      
+      visit "/profile"
+      expect(page).to_not have_link("My Orders")
+
+      @order = user.orders.create!(name: user.name, address: user.address, city: user.city, state: user.state, zip: user.zip)
+
+      visit "/profile"
+      expect(page).to have_link("My Orders")
+      click_on "My Orders"
+      expect(current_path).to eq("/profile/orders")
+    end
   end
 end
