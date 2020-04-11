503121bde91125f1f0f8d10543d878425b9e3205
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-11 11:06:53 -0600

    Add spec for registered user checkout

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe 'Cart show' do
       fill_in :Email, with: user.email
       fill_in :Password, with: user.password
       click_button "Login"    
-                      
+
       visit "/cart"
 
       expect(page).to have_link("Checkout")
@@ -83,4 +83,63 @@ RSpec.describe 'Cart show' do
       end
     end
   end
+
+  describe "As a registered user" do
+    before(:each) do
+      @user = User.create!(name: "Steve",
+                        address:"123 Main St.",
+                        city: "Fort Collins",
+                        state: "GA",
+                        zip: "66666",
+                        email: "chunky_lover@example.com",
+                        password: "123password")
+
+      visit "/login"
+      fill_in :Email, with: @user.email
+      fill_in :Password, with: @user.password
+      click_button "Login"
+
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      visit "/items/#{@paper.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@tire.id}"
+      click_on "Add To Cart"
+      visit "/items/#{@pencil.id}"
+      click_on "Add To Cart"
+      @items_in_cart = [@paper,@tire,@pencil]
+    end
+    
+    it "when I have items in my cart and visit my cart, I see a button to checkout" do
+      visit "/cart"
+      expect(page).to have_button('Checkout Now')
+      click_button('Checkout Now')
+      expect(current_path).to eq("/orders/new")
+      fill_in :name, with: "#{@user.name}"
+      fill_in :address, with: "#{@user.address}"
+      fill_in :city, with: "#{@user.city}"
+      fill_in :state, with: "#{@user.state}"
+      fill_in :zip, with: "#{@user.zip}"
+      click_button('Create Order')
+      expect(page).to have_current_path("/profiles/orders")
+      expect(page).to have_content("Your order was successfully created")
+      expect(page).to have_content("Orders:")
+    end
+  end
 end
+
+# As a registered user
+# When I add items to my cart
+# And I visit my cart
+# I see a button or link indicating that I can check out
+# And I click the button or link to check out and fill out order info and click create order
+# An order is created in the system, which has a status of "pending"
+# That order is associated with my user
+# I am taken to my orders page ("/profile/orders")
+# I see a flash message telling me my order was created
+# I see my new order listed on my profile orders page
+# My cart is now empty
