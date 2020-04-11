28a624b1f0c9c6ae47fd96056374cb3c87b8bd66
Jordan Sewell <jrsewell400@gmail.com>
2020-04-11 14:30:26 -0600

    Update checkout test

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -140,15 +140,3 @@ RSpec.describe 'Cart show' do
     end
   end
 end
-
-# As a registered user
-# When I add items to my cart
-# And I visit my cart
-# I see a button or link indicating that I can check out
-# And I click the button or link to check out and fill out order info and click create order
-# An order is created in the system, which has a status of "pending"
-# That order is associated with my user
-# I am taken to my orders page ("/profile/orders")
-# I see a flash message telling me my order was created
-# I see my new order listed on my profile orders page
-# My cart is now empty
