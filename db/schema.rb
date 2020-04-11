f744be064785e573f324d9f94db368b87bc6f15a
Jordan Sewell <jrsewell400@gmail.com>
2020-04-11 14:30:17 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20200411185852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "status", default: "Pending"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
