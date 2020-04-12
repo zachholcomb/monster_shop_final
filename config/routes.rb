975480a8a81aff8cd0a409a79f4603787ba329bd
Steven Meyers <sjjmeyers@gmail.com>
2020-04-12 11:26:48 -0600

    Add order update route

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
   patch "/profile", to: "users#update"
   get "/profile/orders", to: "orders#index"
   get "/profile/orders/:id", to: 'orders#show'
+  patch "/profile/orders/:id", to: "orders#update"
 
   #merchant
   namespace :merchant do
