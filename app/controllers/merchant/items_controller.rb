761f524bca145e57779aba31e1677e1178c32684
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-13 16:35:50 -0600

    Add update action to merchant items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,10 +2,31 @@ class Merchant::ItemsController < ApplicationController
   before_action :require_merchant
 
   def index
-    @merchant = current_user.merchants[0]
+    @merchant = user_to_merchant
+  end
+
+  def update 
+    merchant = user_to_merchant
+    change_item_status(merchant)
+    redirect_to '/merchant/items'
   end
 
   private
+  
+  def find_item(merchant)
+    merchant.items.find(params[:item_id])
+  end
+
+  def change_item_status(merchant)
+    if params[:type] == 'deactivate'
+      find_item(merchant).update(active?: false)
+      flash[:notice] = "#{find_item(merchant).name} is no longer for sale!"
+    end
+  end
+
+  def user_to_merchant
+    current_user.merchants[0]
+  end
 
   def require_merchant
     render file: "/public/404" unless current_merchant?
