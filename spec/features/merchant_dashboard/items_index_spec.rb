92cbfaa6ec874a9327069c41a124892468fee1cc
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-13 15:57:23 -0600

    Add test for item info on merchant item index page

--- spec/features/merchant_dashboard/items_index_spec.rb
+++ spec/features/merchant_dashboard/items_index_spec.rb
@@ -33,4 +33,54 @@ RSpec.describe "as a merchant employee, when i visit my merchant dashboard", typ
     expect(page).to have_content(pull_toy.name)
     expect(page).to have_content(dog_bone.name)
   end
+
+  it "I see all of my items info" do
+    meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    pull_toy = meg.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = meg.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+    user = meg.users.create!(
+      name: "Steve",
+      address:"123 Main St.",
+      city: "Fort Collins",
+      state: "GA",
+      zip: "66666",
+      email: "chunky_lover@example.com",
+      password: "123password",
+      role: 1
+    )
+
+    visit "/login"
+    fill_in :Email, with: user.email
+    fill_in :Password, with: user.password
+    click_button "Login"
+
+    visit "/merchant/items"
+    
+    within "#item-#{tire.id}" do
+      expect(page).to have_content(tire.name)
+      expect(page).to have_content(tire.description)
+      expect(page).to have_content(tire.price)
+      expect(page).to have_content('Active')
+      expect(page).to have_content(tire.inventory)
+      expect(page).to have_css("img[src*='#{tire.image}']")
+    end
+    within "#item-#{pull_toy.id}" do
+      expect(page).to have_content(pull_toy.name)
+      expect(page).to have_content(pull_toy.description)
+      expect(page).to have_content(pull_toy.price)
+      expect(page).to have_content('Active')
+      expect(page).to have_content(pull_toy.inventory)
+      expect(page).to have_css("img[src*='#{pull_toy.image}']")
+    end
+
+    within "#item-#{dog_bone.id}" do
+      expect(page).to have_content(dog_bone.name)
+      expect(page).to have_content(dog_bone.description)
+      expect(page).to have_content(dog_bone.price)
+      expect(page).to have_content('Inactive')
+      expect(page).to have_content(dog_bone.inventory)
+      expect(page).to have_css("img[src*='#{dog_bone.image}']")
+    end
+  end
 end
