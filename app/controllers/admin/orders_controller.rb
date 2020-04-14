8dfbc0feab6e356f3ca41de85396a2cf011e4c58
Jordan Sewell <jrsewell400@gmail.com>
2020-04-14 11:31:49 -0600

    Update params[:order_id] to just [:id] in update method

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < Admin::BaseController
   def update 
-    order = Order.find(params[:order_id])
+    order = Order.find(params[:id])
     order.update(status: 2)
     redirect_to admin_dashboard_path
   end
