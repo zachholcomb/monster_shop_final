9021e81974fa169bcb1c081cbd9066d6c8831d10
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-14 20:46:27 -0600

    add admin merchants route

--- config/routes.rb
+++ config/routes.rb
@@ -78,6 +78,7 @@ Rails.application.routes.draw do
     get '/users', to: 'users#index'
     get "/users/:user_id", to: "users#show"
     get '/users/:user_id/orders', to: 'users_orders#index'
+    get '/merchants', to: 'merchants#index'
     resources :orders, only: [:update]
     resources :merchants
   end
