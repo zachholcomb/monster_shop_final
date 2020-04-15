7a93b74926861ed459dfe2cb543e591a04127f9e
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-14 20:46:06 -0600

    add patch logic for enable/disable button in admin merchants controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,18 +1,23 @@
 class Admin::MerchantsController < Admin::BaseController
-  def index 
+  def index
     @merchants = Merchant.all
   end
 
-  def show 
+  def show
     @merchant = Merchant.find(params[:id])
   end
 
-  def update 
+  def update
     merchant = Merchant.find(params[:id])
     if merchant.enabled?
       merchant.update(enabled?: false)
       merchant.deactivate_items
       flash[:notice] = "#{merchant.name} is now disabled."
+      redirect_to admin_merchants_path
+    else
+      merchant.update(enabled?: true)
+      merchant.activate_items
+      flash[:notice] = "#{merchant.name} is now enabled."
       redirect_to admin_merchants_path
     end
   end
