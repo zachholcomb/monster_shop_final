4ee82accdbb8aa97a1e1382d4b2184f57cf9802f
Steven Meyers <sjjmeyers@gmail.com>
2020-04-16 11:44:18 -0600

    Deleted duplicate controller actions from brown code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,48 +8,4 @@ class ItemsController<ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def new
-    @merchant = Merchant.find(params[:merchant_id])
-  end
-
-  def create
-    @merchant = Merchant.find(params[:merchant_id])
-    item = @merchant.items.create(item_params)
-    if item.save
-      redirect_to "/merchants/#{@merchant.id}/items"
-    else
-      flash[:error] = item.errors.full_messages.to_sentence
-      render :new
-    end
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to "/items/#{@item.id}"
-    else
-      flash[:error] = @item.errors.full_messages.to_sentence
-      render :edit
-    end
-  end
-
-  def destroy
-    item = Item.find(params[:id])
-    Review.where(item_id: item.id).destroy_all
-    item.destroy
-    redirect_to "/items"
-  end
-
-  private
-
-  def item_params
-    params.permit(:name,:description,:price,:inventory,:image)
-  end
-
-
 end
