af364536138232f42b51224220b3e0a6c207e6a3
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-15 19:27:45 -0600

    add routes for admin edit user profile, password

--- config/routes.rb
+++ config/routes.rb
@@ -78,6 +78,9 @@ Rails.application.routes.draw do
     get "/dashboard", to: "dashboard#show"
     get '/users', to: 'users#index'
     get "/users/:user_id", to: "users#show"
+    get '/users/:user_id/edit/password', to: 'users#edit_password'
+    get '/users/:user_id/edit', to: 'users#edit'
+    patch '/users/:user_id', to: 'users#update'
     get '/users/:user_id/orders', to: 'users_orders#index'
     get '/merchants', to: 'merchants#index'
     resources :orders, only: [:update]
