3f2d87bd9ec1198906650f0eebd617df115f8b3d
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-15 19:28:51 -0600

    add edit, edit_password, update, user_params to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,31 @@ class Admin::UsersController < Admin::BaseController
     @user = User.find(params[:user_id])
   end
 
+  def edit
+    @user = User.find(params[:user_id])
+  end
+
+  def edit_password
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    user = User.find(params[:user_id])
+    if user.update(user_params)
+      if params[:password]
+        flash[:notice] = "Password has been updated!"
+        return redirect_to "/admin/users/#{user.id}"
+      end
+      flash[:notice] = "Profile has been updated!"
+      redirect_to "/admin/users/#{user.id}"
+    else
+      flash[:notice] = user.errors.full_messages.to_sentence
+      redirect_to "/admin/users/#{user.id}/edit"
+    end
+  end
+
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+  end
+
 end
