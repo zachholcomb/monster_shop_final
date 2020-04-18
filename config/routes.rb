68e8fbd0df0f1972f0350cde5499e8c764626b81
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-18 13:39:43 -0600

    Update routes and controller to display right paths

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
+  get "/merchants/:merchant_id/items", to: "merchants_items#index"
+  get "/merchants/:merchant_id/items/:item_id", to: "merchants_items#show"
   post "/merchants", to: "merchants#create"
   get "/merchants/:id/edit", to: "merchants#edit"
   patch "/merchants/:id", to: "merchants#update"
@@ -18,7 +20,7 @@ Rails.application.routes.draw do
   get "/items/:id", to: "items#show"
   get "/items/:id/edit", to: "items#edit"
   patch "/items/:id", to: "items#update"
-  get "/merchants/:merchant_id/items", to: "items#index"
+  
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
   delete "/items/:id", to: "items#destroy"
