fab5815f99be5da257a24ccefb30eceb3d25e981
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-18 22:37:58 -0600

    Add discount show action and test

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,8 @@ class Merchant::DiscountsController < ApplicationController
   def index
     @discounts = current_user.merchants.first.discounts
   end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
 end
