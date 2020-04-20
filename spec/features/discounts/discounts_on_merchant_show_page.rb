06fba59fad77fa418a7d378a28a49f379af9d369
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-20 00:29:31 -0600

    Add test for user seeing discounts on merchant show page

---
+++ spec/features/discounts/discounts_on_merchant_show_page.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "As a user when I visit a merchant's show page" do
+  before(:each) do
+    @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @pull_toy = @meg.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @meg.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+    @discount1 = @meg.discounts.create!(name: "Flash Sale: 10% off 5 items", percentage: 10, item_amount: 5)
+    @discount2 = @meg.discounts.create!(name: "Fifty Percent Off 50 items", percentage: 25, item_amount: 10)
+    @user = @meg.users.create!(
+      name: "Steve",
+      address:"123 Main St.",
+      city: "Fort Collins",
+      state: "GA",
+      zip: "66666",
+      email: "chunky_lover@example.com",
+      password: "123password",
+      role: 0
+    )
+    visit "/login"
+    fill_in :Email, with: @user.email
+    fill_in :Password, with: @user.password
+    click_button "Login"
+  end
+
+  it "I see a section of that page that lists all that merchant's discounts" do
+    visit "/merchants/#{@meg.id}"
+    within "#merchant-discounts" do
+      expect(page).to have_content("We have specials on offer for bulk orders:") 
+      expect(page).to have_content(@discount1.name)
+      expect(page).to have_content(@discount2.name)
+    end
+
+    visit "/merchants/#{@mike.id}"
+
+    within "#merchant-discounts" do
+      expect(page).to have_content("We currently have no specials on offer") 
+    end
+  end
+end
