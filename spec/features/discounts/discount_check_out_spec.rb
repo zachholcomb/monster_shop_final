a1ce835c4d079f898096251345669d283e6c4a2a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-20 16:26:13 -0600

    Add test for discounts on check out page

---
+++ spec/features/discounts/discount_check_out_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user when I have discounts and I checkout' do
+  before(:each) do
+    @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @pull_toy = @meg.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @meg.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+    @discount1 = @meg.discounts.create!(name: "Flash Sale", percentage: 10, item_amount: 5)
+    @discount2 = @meg.discounts.create!(name: "Fifty Percent Off 50 items", percentage: 25, item_amount: 10)
+    @user = User.create!(
+      name: "Steve",
+      address:"123 Main St.",
+      city: "Fort Collins",
+      state: "GA",
+      zip: "66666",
+      email: "chunky_lover@example.com",
+      password: "123password",
+      role: 0
+    )
+    visit "/login"
+    fill_in :Email, with: @user.email
+    fill_in :Password, with: @user.password
+    click_button "Login"
+
+    visit "/items/#{@tire.id}"
+    click_on "Add To Cart"
+
+    visit "/items/#{@dog_bone.id}"
+    click_on "Add To Cart"
+
+    visit "/cart"
+
+    within "#item-quantity-#{@tire.id}" do
+      click_on "Increase Quantity"
+      click_on "Increase Quantity"
+      click_on "Increase Quantity"
+      click_on "Increase Quantity"
+    end
+    click_on 'Checkout'
+  end
+  it 'I see that items with a discounted price carry over' do
+    expect(page).to have_current_path("/orders/new")
+    within("#order-item-#{@tire.id}") do
+      expect(page).to have_content("$90.00")
+      expect(page).to have_content("$450.00") 
+    end
+    expect(page).to have_content("$471.00") 
+  end
+end
