2806e517ba6464d00234cd268b477898401de011
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-21 12:07:27 -0600

    Create discount show spec for admin

---
+++ spec/features/admin/discounts/admin_merchant_show_discounts_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin when I visit a merchants show page' do
+  before(:each) do
+    @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @pull_toy = @meg.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @meg.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+    @discount1 = @meg.discounts.create!(name: "Flash Sale", percentage: 10, item_amount: 5)
+    @discount2 = @meg.discounts.create!(name: "Fifty Percent Off 50 items", percentage: 25, item_amount: 10)
+    @user = User.create!(
+      name: "Steve",
+      address:"123 Main St.",
+      city: "Fort Collins",
+      state: "GA",
+      zip: "66666",
+      email: "chunky_lover@example.com",
+      password: "123password",
+      role: 2
+    )
+    visit "/login"
+    fill_in :Email, with: @user.email
+    fill_in :Password, with: @user.password
+    click_button "Login"
+  end
+
+  it 'I see their discounts listed on their profile page' do
+    visit "/admin/merchants/#{@meg.id}"
+    expect(page).to have_content(@discount1.name) 
+    expect(page).to have_content(@discount2.name) 
+  end
+
+  it "I can click on a discount link and am taken to that discounts show page" do
+    visit "/admin/merchants/#{@meg.id}"
+    click_link @discount1.name
+    expect(page).to have_current_path("/admin/merchants/#{@meg.id}/discounts/#{@discount1.id}") 
+    expect(page).to have_content(@discount1.name)
+  end
+end
