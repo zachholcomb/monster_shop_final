d7b46ba0bd8135ea6d72b71af9492783feb9d9b5
Zach Holcomb <zachholcombmusic@gmail.com>
2020-04-21 19:12:32 -0600

    Address edge case of pertage over 100

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,6 @@
 class Discount < ApplicationRecord
  validates_presence_of :name, :percentage, :item_amount
- validates_numericality_of :percentage, greater_than: 0
+ validates_numericality_of :percentage, greater_than: 0, less_than: 100
  validates_numericality_of :item_amount, greater_than: 1
  belongs_to :merchant
 end
